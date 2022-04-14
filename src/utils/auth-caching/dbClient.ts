050935c384b14fc5053ed4fe00ed6cabf8534b61
Zyriab <flacodevworks@gmail.com>
2022-04-14 15:47:55 +0200

    Update dbClient.ts

--- src/utils/auth-caching/dbClient.ts
+++ src/utils/auth-caching/dbClient.ts
@@ -2,10 +2,10 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 export default function dbClient() {
   return new DynamoDBClient({
-    region: process.env.S3_REGION!,
+    region: process.env.DB_REGION!,
     credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+      accessKeyId: process.env.DB_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.DB_SECRET_ACCESS_KEY!,
     },
   });
 }
