b3cc2bc1acdcfc0f670903a668240a588a26a288
Zyriab <flacodevworks@gmail.com>
2022-05-03 01:40:09 +0200

    Update controlVersions.ts

--- src/utils/s3/controlVersions.ts
+++ src/utils/s3/controlVersions.ts
@@ -17,29 +17,35 @@ export default async function controlVersions(
 
     if (error) throw error as Error;
 
-    const fileToUpdate = (<any[]>files!).filter((f) => f.name === args.fileName);
+    const fileToUpdate = (<any[]>files!).filter(
+      (f) => f.name === args.fileName
+    );
 
-    if (fileToUpdate.length === 0 || fileToUpdate[0].length < args.maxVersionsNumber) {
+    if (
+      fileToUpdate.length === 0 ||
+      fileToUpdate[0].length < args.maxVersionsNumber
+    ) {
       return [undefined, false];
     }
 
     const filesToDelete = await Promise.all(
-      fileToUpdate[0].versions!.map((v: any) =>
-        deleteOneFile({
-          fileName: args.fileName,
-          versionId: v.id,
-          bucketName: args.bucketName,
-        })
-      )
+      fileToUpdate[0]
+        .versions!.sort(
+          (a: any, b: any) =>
+            new Date(a.lastModified).getTime() -
+            new Date(b.lastModified).getTime()
+        )
+        .slice(args.maxVersionsNumber)
+        .map((v: any) =>
+          deleteOneFile({
+            fileName: args.fileName,
+            versionId: v.id,
+            bucketName: args.bucketName,
+          })
+        )
     );
 
-    if (filesToDelete.filter(([e]) => e !== undefined).length > 0) {
-      for (const [e] of filesToDelete) {
-        if (e) throw e;
-      }
-    }
-
-    return [undefined, true];
+    return [filesToDelete.filter(([e]) => e !== undefined)[0][0]!, true];
   } catch (err) {
     return [err as Error];
   }
