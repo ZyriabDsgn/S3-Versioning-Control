1c2ad6fcaf0d4dd758a76ddd0776734630dae74c
Zyriab <flacodevworks@gmail.com>
2022-05-02 08:13:52 +0200

    Create deleteOneFile.ts

---
+++ src/utils/s3/deleteOneFile.ts
@@ -0,0 +1,45 @@
+import {
+  DeleteObjectCommand,
+  DeleteObjectCommandOutput,
+} from '@aws-sdk/client-s3';
+import isBucketVersioned from './isBucketVersioned';
+import s3Client from './s3Client';
+
+interface InputArgs {
+  fileName: string;
+  bucketName: string;
+  versionId?: string;
+}
+
+export default async function deleteOneFile(
+  args: InputArgs
+): Promise<[undefined, string] | [Error]> {
+  try {
+    let res: DeleteObjectCommandOutput | undefined;
+    const isVersioned = await isBucketVersioned(args.bucketName);
+
+    const params: any = {
+      Bucket: args.bucketName,
+      Key: `${args.fileName}`,
+      VersionId: isVersioned ? args.versionId : 'null',
+    };
+
+    if (!isVersioned) {
+      res = await s3Client().send(new DeleteObjectCommand(params));
+    } else if (isVersioned && args.versionId) {
+      res = await s3Client().send(new DeleteObjectCommand(params));
+    }
+
+    const status = res?.$metadata.httpStatusCode || 500;
+
+    if (status >= 200 && status <= 299) {
+      return [undefined, args.fileName];
+    }
+
+    throw new Error(
+      `Could not finish deletion: ${status}. Some objects may have been deleted.`
+    );
+  } catch (err) {
+    return [err as Error];
+  }
+}
